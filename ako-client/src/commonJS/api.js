67d9d210dc002bd0d03ade665347e3f82e3f1282
nojaewon <nojy99wodnjs@gmail.com>
2023-04-04 15:53:37 +0900

    회원가입 페이지 UI 변경 및 회원가입 API 생성

--- ako-client/src/commonJS/api.js
+++ ako-client/src/commonJS/api.js
@@ -6,11 +6,11 @@ const api = {
     // 로그인 api
     // { usrId, usrPwd } => { OUTPUT }
     requestLogin : async (id, pw)=>{
-        const login_success_url = `${URL}/member/login`;
+        const login_request_url = `${URL}/member/login`;
 
         // json-server를 사용할 시에 axios.post로 요청을 보내면 db.json data를 잃는 이슈
         // const accToken = await axios.post(login_success_url, {
-        const accToken = await axios.get(login_success_url, {
+        const result = await axios.get(login_request_url, {
             "usrId": id,
             "usrPwd": pw
         })
@@ -32,10 +32,27 @@ const api = {
                 }
             })
         
-        return accToken;
+        return result;
     },
+
+    requestSignUp: async (formData)=>{
+        // 회원가입 api
+        const signup_request_url = `${URL}/member/join`;
+
+
+        // json-server를 사용할 시에 axios.post로 요청을 보내면 db.json data를 잃는 이슈
+        // const accToken = await axios.post(login_success_url, {
+            const result = await axios.get(signup_request_url, formData)
+                .then((response)=>{
+                    return response.data
+                })
+            
+            return result;
+    }
 
 
 };
+
+
 
 export default api;
--- ako-client/src/components/RspSignUpContainer.js
+++ ako-client/src/components/RspSignUpContainer.js
@@ -1,7 +1,16 @@
+//react
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+
+// style
 import '../css/RspSignUpContainer.css'
+
+// import commonJS module
+import api from '../commonJS/api.js';
 
 // antd design
 import {
+    Modal,
     Button,
     Checkbox,
     Form,
@@ -10,6 +19,7 @@ import {
 } from 'antd';
 
 const { Option } = Select;
+
 const formItemLayout = {
   labelCol: {
     xs: {
@@ -42,12 +52,30 @@ const tailFormItemLayout = {
   },
 };
 
+// API 명세에 따른 폼 name value 설정
+const USRFORM = {
+    id: "usrId",
+    pw: "usrPwd",
+    email: 'usrEmail',
+    name: 'usrNm',
+    tel: 'usrTelNo',
+    nickname: 'usrNickName'
+}
+
 function RspSignUpContainer(){
+    const navigate = useNavigate();
     const [form] = Form.useForm();
+    const [isContinueModal, setIsContinueModal] = useState(false);
     
     // 폼 제출 시 실행 함수
     const onFinish = (values) => {
-        console.log('Received values of form: ', values);
+        api.requestSignUp(values).then((data)=>{
+            if(data.state === 200){
+                console.log(data.message);
+            } else if (data.state === 400){
+                console.log(data.message);
+            }
+        });
     };
 
     // 전화번호 
@@ -68,134 +96,147 @@ function RspSignUpContainer(){
             <Form
                 {...formItemLayout}
                 form={form}
-                name="register"
                 onFinish={onFinish}
                 initialValues={{
-                    residence: ['zhejiang', 'hangzhou', 'xihu'],
                     prefix: '86',
                 }}
                 style={{maxWidth: 600,}}
                 scrollToFirstError
             >
-            <Form.Item
-                name="email"
-                label="E-mail"
-                rules={[
-                {
-                    type: 'email',
-                    message: 'The input is not valid E-mail!',
-                },
-                {
-                    required: true,
-                    message: 'Please input your E-mail!',
-                },
-                ]}
-            >
-                <Input />
-            </Form.Item>
-
-            <Form.Item
-                name="password"
-                label="Password"
-                rules={[
-                {
-                    required: true,
-                    message: 'Please input your password!',
-                },
-                ]}
-                hasFeedback
-            >
-                <Input.Password />
-            </Form.Item>
-
-            <Form.Item
-                name="confirm"
-                label="Confirm Password"
-                dependencies={['password']}
-                hasFeedback
-                rules={[
+                <Form.Item
+                    name={USRFORM.id}
+                    label="아이디"
+                    rules={[
+                    {
+                        type: 'id',
+                        message: '유효한 아이디가 아닙니다!',
+                    },
                     {
                         required: true,
-                        message: 'Please confirm your password!',
+                        message: '아이디를 입력해주세요!',
                     },
-                    ({ getFieldValue }) => ({
-                        validator(_, value) {
-                        if (!value || getFieldValue('password') === value) {
-                            return Promise.resolve();
-                        }
-                        return Promise.reject(new Error('The two passwords that you entered do not match!'));
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    name={USRFORM.email}
+                    label="이메일"
+                    rules={[
+                    {
+                        type: 'email',
+                        message: '유효한 이메일이 아닙니다!',
+                    },
+                    {
+                        required: true,
+                        message: '이메일을 입력해주세요!',
+                    },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    name={USRFORM.pw}
+                    label="비밀번호"
+                    rules={[
+                    {
+                        required: true,
+                        message: '비밀번호를 입력해주세요!',
+                    },
+                    ]}
+                    hasFeedback
+                >
+                    <Input.Password />
+                </Form.Item>
+
+                <Form.Item
+                    name="confirm"
+                    label="비밀번호 확인"
+                    dependencies={[USRFORM.pw]}
+                    hasFeedback
+                    rules={[
+                        {
+                            required: true,
+                            message: '비밀번호를 확인해주세요!',
                         },
-                    }),
-                ]}
-            >
-                <Input.Password />
-            </Form.Item>
+                        ({ getFieldValue }) => ({
+                            validator(_, value) {
+                            if (!value || getFieldValue(USRFORM.pw) === value) {
+                                return Promise.resolve();
+                            }
+                            return Promise.reject(new Error('비밀번호가 일치하지 않습니다!'));
+                            },
+                        }),
+                    ]}
+                >
+                    <Input.Password />
+                </Form.Item>
 
-            <Form.Item
-                name="nickname"
-                label="Nickname"
-                tooltip="What do you want others to call you?"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your nickname!',
-                        whitespace: true,
-                    },
-                ]}
-            >
-                <Input />
-            </Form.Item>
+                <Form.Item
+                    name={USRFORM.nickname}
+                    label="닉네임"
+                    tooltip="다른 사람들이 당신을 어떤 이름으로 불렀으면 하나요?"
+                    rules={[
+                        {
+                            required: false,
+                            message: '당신의 닉네임을 설정해주세요!',
+                            whitespace: true,
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
 
-            <Form.Item
-                name="phone"
-                label="Phone Number"
-                rules={[
+                <Form.Item
+                    name={USRFORM.tel}
+                    label="핸드폰 번호"
+                    rules={[
+                        {
+                            required: true,
+                            message: '핸드폰 번호를 입력해주세요!',
+                        },
+                    ]}
+                >
+                    <Input
+                    addonBefore={prefixSelector}
+                    style={{ width: '100%',}}
+                    />
+                </Form.Item>
+                    
+                <Form.Item
+                    name="agreement"
+                    valuePropName="checked"
+                    rules={[
                     {
-                        required: true,
-                        message: 'Please input your phone number!',
+                        validator: (_, value) =>
+                        value ? Promise.resolve() : Promise.reject(new Error('해당 항목을 동의해주세요')),
                     },
-                ]}
-            >
-                <Input
-                addonBefore={prefixSelector}
-                style={{ width: '100%',}}
-                />
-            </Form.Item>
-
-            <Form.Item
-                name="intro"
-                label="Intro"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input Intro',
-                    },
-                ]}
-            >
-                <Input.TextArea showCount maxLength={100} />
-            </Form.Item>
-                
-            <Form.Item
-                name="agreement"
-                valuePropName="checked"
-                rules={[
-                {
-                    validator: (_, value) =>
-                    value ? Promise.resolve() : Promise.reject(new Error('Should accept agreement')),
-                },
-                ]}
-                {...tailFormItemLayout}
-            >
-                <Checkbox>
-                I have read the <a href="">agreement</a>
-                </Checkbox>
-            </Form.Item>
-            <Form.Item {...tailFormItemLayout}>
-                <Button type="primary" htmlType="submit">
-                Register
-                </Button>
-            </Form.Item>
+                    ]}
+                    {...tailFormItemLayout}
+                >
+                    <Checkbox>
+                    위 개인정보를 수집&middot;이용하는 것에 동의합니다.
+                    </Checkbox>
+                </Form.Item>
+                <Form.Item {...tailFormItemLayout}>
+                    <Button type="primary" htmlType="submit" style={{width: 100}} >계정 만들기</Button>
+                    <Button type="default" style={{marginLeft: 30, width: 100}} onClick={()=>{
+                        setIsContinueModal(true);
+                    }}>뒤로 가기</Button>
+                </Form.Item>
             </Form>
+            <Modal title="주의" open={isContinueModal}
+                onOk={()=>{
+                    navigate('/');
+                }}
+                onCancel={()=>{
+                    setIsContinueModal(false);
+                }}
+            >
+                    <p>기록했던 모든 정보는 저장되지 않습니다. 뒤로 가시겠습니까?</p>
+            </Modal>
         </div>
        
     );
