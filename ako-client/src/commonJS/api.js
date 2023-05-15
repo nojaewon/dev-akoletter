43906fba9619826b561ccf41d03d6879e503a963
nojaewon <nojy99wodnjs@gmail.com>
2023-05-15 14:27:10 +0900

    프록시 재 설정 및 api LOCAL 상수값 수정

--- ako-client/src/commonJS/api.js
+++ ako-client/src/commonJS/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const LOCAL = true;
+const LOCAL = true; // 로컬 서버를 사용할 경우 true
+const useJSONServer = true; // 서버를 임의의 Json-server로 사용할 시 true 아닐 시 false
+
 const URL = LOCAL ? "http://localhost:8080" : "https://dev-akoletter-api.herokuapp.com";
 
 const api = {
@@ -9,7 +11,7 @@ const api = {
         const login_request_url = `${URL}/member/login`;
         let result;
 
-        if(LOCAL){
+        if(useJSONServer){
             result = await axios.get(login_request_url)
                 .then((response)=>{
                     const data = response.data.data;
@@ -21,7 +23,7 @@ const api = {
                     }
                 })
         } else {
-            result = await axios.axios.post(login_request_url, {
+            result = await axios.post(login_request_url, {
                 "usrId": id,
                 "usrPwd": pw
              })
@@ -52,7 +54,7 @@ const api = {
         const signup_request_url = `${URL}/member/join`;
 
         let result;
-        if(LOCAL){
+        if(useJSONServer){
             result = await axios.get(signup_request_url).then(res=>res.data);
         } else {
             result = await axios.post(signup_request_url, formData).then(res=>res.data);
@@ -65,7 +67,7 @@ const api = {
         // 원문 요약 API
         const summary_request_url = `${URL}/editor/summary`;
         let result;
-        if(LOCAL){
+        if(useJSONServer){
             result = await axios.get(summary_request_url).then(res=>res.data);
         } else {
             result = await axios.post(summary_request_url, formData).then(res=>res.data);
@@ -81,8 +83,9 @@ const api = {
 
     getPostDetail: async(id)=>{
         const postdetail_request_url = `${URL}/post/postdetail/${id}`;
+        
         let result;
-        if(LOCAL){
+        if(useJSONServer){
             result = await axios.get(postdetail_request_url).then(res=>res.data);
         } else {
             result = await axios.post(postdetail_request_url, {"id": id}).then(res=>res.data);
